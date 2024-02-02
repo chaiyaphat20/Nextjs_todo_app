aeb9dd5cdd96e9e1e243d668d6e57f80a4535737
chaipat supharak <chaipat.sup@cpf.co.th>
2024-02-02 22:00:23 +0700

    feat: add config redux toolkit

--- src/app/[locale]/(dashboard)/todo/page.tsx
+++ src/app/[locale]/(dashboard)/todo/page.tsx
@@ -1,27 +1,9 @@
 'use client'
-import { TodoItemType } from '@/app/[locale]/types/todo-type'
-import { useEffect, useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
 import AddButton from '../../components/todo-add-button'
 import TodoItem from '../../components/todo-item'
 import TodoTitle from '../../components/todo-title'
 
 export default function Main() {
-  const [todoList, setTodoList] = useState<TodoItemType[]>([])
-  useEffect(() => {
-    const arr: TodoItemType[] = []
-    for (let index = 0; index < 5; index++) {
-      const data: TodoItemType = {
-        id: uuidv4(),
-        checked: false,
-        todo: `${index} data test`
-      }
-      arr.push(data)
-    }
-    setTodoList(arr)
-  }, [])
-
-  console.log(todoList)
   return (
     <div className="flex flex-grow items-center justify-center h-full text-gray-600 bg-gray-100">
       <div className="max-w-full p-8 bg-white rounded-lg shadow-lg w-96">
